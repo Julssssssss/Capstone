872b1bfc66e417b2238609bd6e1abd8179c52a54
Glen Danielle D. Galit <glendanielleg@gmail.com>
2023-08-31 21:00:33 +0800

    initialized back end

---
+++ backEnd/server.js
@@ -0,0 +1,13 @@
+const express = require('express') //npm i express cors
+const cors = require('cors')
+const app = express(); //to use express
+const port = 3000;
+
+app.use(cors()) //to allow everyone access the cors will change later 
+
+app.listen(port, console.log(`running in port ${port}`)) //run the port in 3000
+
+app.use((req, res)=>{
+    //if you made a mistake on typing the url
+    res.status(404).send(`<h1>WRONG URL!</h1>`)
+})
