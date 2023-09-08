2a9e2ea094af96cde2a61ae189e872c117d5783b
Glen Danielle D. Galit <glendanielleg@gmail.com>
2023-09-08 10:16:48 +0800

    fixed the dashboard dataflow and layout

--- frontEnd/src/pages/Dashboard.jsx
+++ frontEnd/src/pages/Dashboard.jsx
@@ -25,8 +25,18 @@ const Dashboard = () => {
     return data.map((elem) => 
       Object.values(elem).map((el, index)=>{
         return (
-          <div key={index}>
-            <h1>{el.img}</h1>
+          <div key={index} className="flex flex-row mb-[1rem]">
+            <div>
+              <img src={el.img} alt={el.title}>
+              </img>
+            </div>
+
+            <div className="flex flex-col w-full items-center relative">
+              <div>{el.title}</div>
+              <div className="w-full h-full p-2">{el.desc}</div>
+              
+              <button className={`rounded-full h-2 w-2 absolute bottom-1 right-1 ${el.availability===("yes") ? 'bg-green-500':'bg-red-500'}`}></button>
+            </div>
           </div>
         )
       })
@@ -35,7 +45,24 @@ const Dashboard = () => {
 
   return (
     <div>
-      {sample()}
+      {/*wag kalimutan delete to after kasi pang debug lang tong style nato*/}
+      <style>
+        {`
+          *{outline: solid red}
+        `}
+      </style>
+
+      <div className="bg-[#0d1832] text-white">
+        <div className="flex flex-row">
+          <h1>
+            NAVBAR
+          </h1>
+        </div>
+
+        <div className="flex flex-col h-full p-[1rem]">
+          {sample()}
+        </div>
+      </div>
     </div>
   )
 }
