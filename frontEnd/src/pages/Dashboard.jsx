41540ed89e9fda384b995492bf129a67d70c0b22
Glen Danielle D. Galit <glendanielleg@gmail.com>
2023-10-09 22:48:28 +0800

    fixing bugs in dashboard.jsx because the map isnt working yet

--- frontEnd/src/pages/Dashboard.jsx
+++ frontEnd/src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ import { Link} from "react-router-dom"
 //tumigil ka sa pag console log ng data
 const Dashboard = () => {
   //taga salo lang ng data galing backend
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([null]);
 
   // Create an asynchronous function within useEffect
   //eto yung sa data like items eme
@@ -17,7 +17,7 @@ const Dashboard = () => {
     try {
       const response = await axios.get(`${import.meta.env.VITE_API_URL}/auth/login/success`, {withCredentials: true});
       //setUser([response.data.user])
-      setData(response.data)
+      setData([{'items': response.data.items, 'user':response.data.user}])
     } catch (error) {
       console.log("Error fetching data", error);
     }
@@ -29,7 +29,34 @@ const Dashboard = () => {
     //lagay ka sana dito ng if wala laman user and items go to '/'
   }, []); // Empty dependency array means this effect will run once on mount
 
-  console.log(data.items)
+function sample() {
+  return data.map((elem) =>
+    Object.values(elem.items).map((el, index)=>{
+      console.log(el)
+      return (
+        // container for item and description
+        <div key={index} className="flex flex-row m-3 rounded-lg mb-[1rem] z-0 justify-end h-[9rem] items-center">
+            {/*title container*/}
+          <Link to={`/Item/${el._id}`}>
+            <div className="m-2 rounded-lg bg-[#003985] hover:bg-sky-700 active:bg-[#0d1832] overflow-hidden w-[15rem] h-[5rem]">
+              <div className="flex items-center font-bold text-white ml-[5rem] h-full p-3">
+                {el.title}
+              </div>
+            </div>
+          </Link>
+
+          <div className="p-2 m-3 rounded-full bg-yellow-400 overflow-hidden absolute left-[1rem]">
+            <img src={el.img} alt={el.title} className="rounded-full object-contain w-[7rem]"/>
+          </div>
+          
+        </div>
+      )
+    })
+  );
+}
+
+return sample()
+
 }
 
 export default Dashboard
