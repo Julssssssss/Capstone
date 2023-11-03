5cbf77fe20c3131179ef7ba1c12eb9b1ccab8dfc
Glen Danielle D. Galit <glendanielleg@gmail.com>
2023-11-03 16:37:34 +0800

    fixed the pop-up interaction of terms and agreement

--- frontEnd/src/pages/LandingPage/components/Signup.jsx
+++ frontEnd/src/pages/LandingPage/components/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
       <button onClick={()=>{setShowTAC(!showTAC)}} className="mt-[1.7rem] bg-[#003985] h-[3rem] w-[9rem] p-1 rounded-xl text-white mx-1">Sign up</button>
         {showTAC ? 
           <div>
-            <div></div>
+            <div onClick={()=>{setShowTAC(false)}} className="fixed inset-0 z-10"></div>
             <TermsAndAgreement/>
           </div>
           : null}
